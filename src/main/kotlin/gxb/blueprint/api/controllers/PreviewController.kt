3b67c87febfdad1a8fcf479971bd849baec94bc0
ichos12 <magus78@yandex.ru>
2020-09-04 18:16:13 +0300

    edit

--- src/main/kotlin/gxb/blueprint/api/controllers/PreviewController.kt
+++ src/main/kotlin/gxb/blueprint/api/controllers/PreviewController.kt
@@ -2,87 +2,43 @@ package gxb.blueprint.api.controllers
 
 import gxb.blueprint.api.services.PreviewService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.*
+import org.springframework.http.HttpEntity
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 
 @RestController
-@RequestMapping("api/v1/preview")
+@RequestMapping("api/v1/preview/")
 class PreviewController {
     @Autowired
     lateinit var PreviewService: PreviewService
     lateinit var cont: HttpEntity<*>
 
-    @PostMapping("temp")
-    fun postTemp(@RequestBody content: String): ResponseEntity<*> {
-        cont = ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body("""
-                <!DOCTYPE html>
-                <html>
-                    <head>
-                        <meta charset="utf-8">
-                        <title>Пример страницы</title>
-                        <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-                        <style>
-                            html {
-                              height: 100%;
-                            }
-                            body {
-                              margin: 0;
-                              height: 100%;
-                            }
-                    
-                            /* Tell Quill not to scroll */
-                            #quill-container {
-                              height: auto;
-                              min-height: 100%;
-                              padding: 50px;
-                            }
-                            #quill-container .ql-editor {
-                              font-size: 18px;
-                              overflow-y: visible; 
-                            }
-                    
-                            /* Specify our own scrolling container */
-                            #scrolling-container {
-                              height: 100%;
-                              min-height: 100%;
-                              overflow-y: auto;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <div id="scrolling-container">
-                            <div id="quill-container">
-                       
-                            </div>
-                        </div>
-                    
-                        <!-- Include the Quill library -->
-                        <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
-                    
-                        <!-- Initialize Quill editor -->
-                        <script>
-                            var quill = new Quill('#quill-container', {
-                            modules: {
-                              toolbar: false
-                            },
-                            readOnly: true,
-                            scrollingContainer: '#scrolling-container', 
-                            placeholder: 'Compose an epic...',
-                            theme: 'bubble'
-                            });
-                    
-                            quill.setContents($content)
-                        </script>
-                    </body>
-                </html>
-                """)
-        val response = PreviewService.makeScreenshot()
+    @PostMapping("/")
+    fun process(@RequestBody content: String, model: Model, @ModelAttribute data: HttpEntity<*>): ResponseEntity<*> {
+        val requestUri = ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString()
+        println(requestUri)
+        cont = PreviewService.getEntity(content)
+        //model["data"] = pageContent
+        //println("post $pageContent")
+        //println("model $model")
+        val response = PreviewService.makeScreenshot(requestUri)
         return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(response)
     }
 
-    @GetMapping("temp")
-    fun temp(): HttpEntity<*> {
+    @GetMapping("/")
+    fun temp(@ModelAttribute data: HttpEntity<*>, model: Model): HttpEntity<*> {
+        val requestUri1 = ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString()
+        println(requestUri1)
+        //println("content1 $model")
+
+        //model["data"] = data
+        //println("content2 $model")
+
         return cont
     }
+
 }
--- src/main/kotlin/gxb/blueprint/api/services/PreviewService.kt
+++ src/main/kotlin/gxb/blueprint/api/services/PreviewService.kt
@@ -5,14 +5,18 @@ import org.openqa.selenium.TakesScreenshot
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.springframework.http.HttpEntity
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import java.awt.Dimension
 import java.awt.Toolkit
 
-
 @Service
 class PreviewService {
-    fun makeScreenshot(): ByteArray {
+    private final val driver: WebDriver
+
+    init {
         val chromeDriverPath: String = "src/main/resources/chromedriver.exe"
         System.setProperty("webdriver.chrome.driver", chromeDriverPath)
         System.setProperty("java.awt.headless", "false");
@@ -21,10 +25,76 @@ class PreviewService {
         val height: Int = sSize.height
         val options: ChromeOptions = ChromeOptions()
         options.addArguments("--headless", "--disable-gpu", "--window-size=$width,$height")
-        val driver: WebDriver = ChromeDriver(options)
-        driver.get("http://localhost:8080/api/v1/preview/temp")
+        driver = ChromeDriver(options)
+    }
+    fun makeScreenshot(urlValue: String?): ByteArray {
+        driver.get("$urlValue")
         val screenshot = (driver as TakesScreenshot).getScreenshotAs(OutputType.BYTES)
-        driver.quit()
+        driver.close();
         return screenshot
+    }
+    fun getEntity(content: String?): HttpEntity<*> {
+         return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body("""
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                        <title>Пример страницы</title>
+                        <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+                        <style>
+                            html {
+                              height: 100%;
+                            }
+                            body {
+                              margin: 0;
+                              height: 100%;
+                            }
+                    
+                            /* Tell Quill not to scroll */
+                            #quill-container {
+                              height: auto;
+                              min-height: 100%;
+                              padding: 50px;
+                            }
+                            #quill-container .ql-editor {
+                              font-size: 18px;
+                              overflow-y: visible; 
+                            }
+                    
+                            /* Specify our own scrolling container */
+                            #scrolling-container {
+                              height: 100%;
+                              min-height: 100%;
+                              overflow-y: auto;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <div id="scrolling-container">
+                            <div id="quill-container">
+                       
+                            </div>
+                        </div>
+                    
+                        <!-- Include the Quill library -->
+                        <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+                    
+                        <!-- Initialize Quill editor -->
+                        <script>
+                            var quill = new Quill('#quill-container', {
+                            modules: {
+                              toolbar: false
+                            },
+                            readOnly: true,
+                            scrollingContainer: '#scrolling-container', 
+                            placeholder: 'Compose an epic...',
+                            theme: 'bubble'
+                            });
+                    
+                            quill.setContents($content)
+                        </script>
+                    </body>
+                </html>
+                """)
     }
 }
